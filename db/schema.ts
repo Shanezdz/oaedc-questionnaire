498596533668d39189cf515ba4baea9c9e7afa1f
Kechroud <122513464+Shanezdz@users.noreply.github.com>
2026-09-08 10:58:37 +0100

    Remove obsolete Drizzle schema

--- db/schema.ts
+++
@@ -1,2 +0,0 @@
-import {sqliteTable,text} from "drizzle-orm/sqlite-core";
-export const responses=sqliteTable("responses",{id:text("id").primaryKey(),createdAt:text("created_at").notNull(),answers:text("answers").notNull()});
